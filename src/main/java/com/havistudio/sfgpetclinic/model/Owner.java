e37d6e4b84490bfa3431ea0758a80f6c43d8611f
Kostas <chaviarask02@gmail.com>
2020-09-26 15:16:54 +0300

    adding POJOs.

---
+++ src/main/java/com/havistudio/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.havistudio.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/havistudio/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.havistudio.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
