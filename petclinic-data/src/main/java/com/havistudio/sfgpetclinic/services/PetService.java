d0868052ca61c7aebebd64ef4e8bcec8d1d40761
Chaviaras Konstantinos <chaviarask02@gmail.com>
2020-09-29 20:23:31 +0300

    refactoring to common crud interface. closes #11

--- petclinic-data/src/main/java/com/havistudio/sfgpetclinic/services/PetService.java
+++ petclinic-data/src/main/java/com/havistudio/sfgpetclinic/services/PetService.java
@@ -2,14 +2,6 @@ package com.havistudio.sfgpetclinic.services;
 
 import com.havistudio.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 
 }
