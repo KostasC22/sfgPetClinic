83e4f022fdecc490f32cf23a3a0f0580c2502732
Kostas <chaviarask02@gmail.com>
2020-11-15 15:04:52 +0200

    122. Spring Pet Clinic - Pet Type Data on Startup

--- petclinic-web/src/main/java/com/havistudio/sfgpetclinic/boostrap/DataLoader.java
+++ petclinic-web/src/main/java/com/havistudio/sfgpetclinic/boostrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.havistudio.sfgpetclinic.boostrap;
 
 import com.havistudio.sfgpetclinic.model.Owner;
+import com.havistudio.sfgpetclinic.model.PetType;
 import com.havistudio.sfgpetclinic.model.Vet;
 import com.havistudio.sfgpetclinic.services.OwnerService;
+import com.havistudio.sfgpetclinic.services.PetTypeService;
 import com.havistudio.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService){
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService){
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType saveCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         Owner owner2 = new Owner();
