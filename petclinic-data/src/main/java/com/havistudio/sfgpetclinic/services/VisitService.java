e03da6a9a0d8eac8279023ac9d36d001a7769e05
Chaviaras Konstantinos <chaviarask02@gmail.com>
2021-09-04 18:58:41 +0300

    157. Spring Pet Clinic - Add Map Based Visit Service

---
+++ petclinic-data/src/main/java/com/havistudio/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.havistudio.sfgpetclinic.services;
+
+import com.havistudio.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ petclinic-data/src/main/java/com/havistudio/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,46 @@
+package com.havistudio.sfgpetclinic.services.map;
+
+import com.havistudio.sfgpetclinic.model.Visit;
+import com.havistudio.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalida Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
