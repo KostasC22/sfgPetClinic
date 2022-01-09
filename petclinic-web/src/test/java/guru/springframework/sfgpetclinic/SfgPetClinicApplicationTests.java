5c4ea1ea5f7fab1cecf88293ddb70bbde3daf985
Chaviaras Konstantinos <chaviarask02@gmail.com>
2022-01-09 20:45:18 +0200

    183. Spring Pet Clinic - Convert to JUnit 5

--- petclinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ petclinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
 package guru.springframework.sfgpetclinic;
 
+import com.havistudio.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = SfgPetClinicApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SfgPetClinicApplicationTests {
 
     @Test
